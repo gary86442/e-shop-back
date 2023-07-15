f85436c029773733cb43ba1bff8da9f4a810e1b1
Leechiayu <gary86442@gmail.com>
2023-07-15 12:40:56 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,16 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const express = require('express')
+const routes = require('./routes')
+const app = express()
+
+const port = process.env.PORT | 3000
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+app.use(routes)
+app.listen(port, () => {
+  console.log(`server start on http://localhost:${port}`)
+})
